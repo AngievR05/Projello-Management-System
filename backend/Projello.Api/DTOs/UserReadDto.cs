11eba6975ae6ece71cdb1d8677c6ed6767076e83
200238_David Golding <200238@virtualwindow.co.za>
2026-04-16 20:08:23 +0200

    Update to userdto

--- backend/Projello.Api/DTOs/UserReadDto.cs
+++ backend/Projello.Api/DTOs/UserReadDto.cs
@@ -9,5 +9,16 @@ namespace Projello.Api.DTOs;
         public string Email { get; set; } = null!;
         public string Role { get; set; } = null!;
         public DateTime CreatedAt { get; set; }
+
+        public bool isTwoFactorEnabled { get; set; } 
+
+        public class VerifyTwoFactorDto
+        {
+            [Required]
+            public string Email { get; set; } = null!;
+
+            [Required]
+            public string Code { get; set; } = null!;
+        }
     }
  // previous version allowed you type 123 for example as a valid email. new version requires correct email format.
